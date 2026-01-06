9d53a4ff1ba3725d1b40055fcda95040a05d4f78
Edilberto Martinez Diaz <edilberto_diaz14@hotmail.com>
2026-01-06 16:45:00 -0600

    Adds category to the proveedor entity
    Adds a category property to the proveedor entity.
    This allows for better categorization and filtering
    of proveedores.
    
    Relates to WTBA-125

---
+++ Wallet.DOM/Migrations/20260106224400_ProveedorAddCategoria.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wallet.DOM.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProveedorAddCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Categoria",
+                table: "Proveedor",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Proveedor");
+        }
+    }
+}
